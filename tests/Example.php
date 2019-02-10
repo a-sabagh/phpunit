536be0658a8d5acc71a7b605f471aef338c5ec36
asabagh <a.sabagh72@gmail.com>
2019-02-10 14:08:54 +0330

    first test

---
+++ tests/Example.php
@@ -0,0 +1,29 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class Example extends TestCase
+{
+	/** @test */
+    public function one()
+    {
+        $this->assertTrue(true);
+    }
+	
+	/** @test */
+    public function tow()
+    {
+        $this->assertTrue(true);
+    }
+	
+	/** @test */
+    public function three()
+    {
+        $this->assertTrue(true);
+    }
+	
+	/** @test */
+    public function four()
+    {
+        $this->assertTrue(true);
+    }
+}
